5993df8ea784b8ad7f05c9b9b8b6ad7cb77ef848
henry.zhang <1002527441@qq.com>
2022-07-07 20:34:21 +0800

    添加项目文件。

---
+++ MiniExcelApp/Part.cs
@@ -0,0 +1,24 @@
+﻿using MiniExcelLibs.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniExcelApp
+{
+    public class Part
+    {
+        [ExcelColumnName("ID")]
+        public int Id { get; set; }
+
+        [ExcelIgnore]
+        public DateTime Birth { get; set; }
+
+        [ExcelColumnName("Part number")]
+        public string? Partnumber { get; set; }
+
+        [ExcelColumnName("Lot number")]
+        public string?  Lotnumber { get; set; }
+    }
+}
---
+++ MiniExcelApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MiniExcelApp;
+using MiniExcelLibs;
+
+Console.WriteLine("Hello, World!");
+var path = "c:\\temp\\test1.xlsx";
+foreach (dynamic r in MiniExcel.Query(path))
+{
+    Console.WriteLine($"{r.A}|{r.B}|{r.C}|{r.D}");
+}
+
+foreach (var r in MiniExcel.Query<Part>(path))
+{
+    Console.WriteLine($"{r.Id}|{r.Partnumber}|{r.Lotnumber}|{r.Birth}");
+}
+
+Console.ReadLine();
